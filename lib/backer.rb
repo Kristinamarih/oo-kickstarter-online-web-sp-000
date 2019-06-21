ab810c00ade8d46c1912cbfd1ea56390f208ff17
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-06-21 17:42:58 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -13,6 +13,6 @@ class Backer
   
   def back_project(project)
     @backed_projects << project
-    @name.each do {|name| Project.add_backer(backer)}
+    backer.each do {|backer| Project.add_backer(backer)}
   end
 end
